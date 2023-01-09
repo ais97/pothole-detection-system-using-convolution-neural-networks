bea81a169685a915f91b46675d16f51d210969f1
Vaidyanathan Aiswarya <49706145+ais97@users.noreply.github.com>
2023-01-09 11:22:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
 
 import tensorflow as tf
-from tensorflow.keras.layers import flatten
+from tensorflow.compat.v1.layers import flatten
 
 from keras.layers.pooling import MaxPooling2D
 from keras.models import Sequential, Model
